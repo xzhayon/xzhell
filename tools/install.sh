2b1dee155d3f55555ee5a20026c22337dcf99c57
Alberto Villa <villa.alberto@gmail.com>
2018-02-13 18:27:23 +0100

    Ensure compatibility

--- tools/install.sh
+++ tools/install.sh
@@ -15,7 +15,7 @@ hash git >/dev/null 2>&1 || {
   else
 	env git clone --depth=1 $REMOTE $LOCAL
   fi ) &&
-sed -i '' 's,source \${0%/\*}/\.\.,source '$LOCAL',' $LOCAL/bin/realpath &&
+sed -i.orig 's,source \${0%/\*}/\.\.,source '$LOCAL',' $LOCAL/bin/realpath &&
 mkdir -p $HOME/bin && {
 	for file in $LOCAL/bin/*; do
 		ln -s $LOCAL/bin/${file##*/} bin/ 2>/dev/null || true
