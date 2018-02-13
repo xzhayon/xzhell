a63db25c5a76cf2397ef5d8aca6b6a9e174f1abd
Alberto Villa <villa.alberto@gmail.com>
2018-02-13 18:22:59 +0100

    Try replacing relative path with absolute path on installation

--- tools/install.sh
+++ tools/install.sh
@@ -10,10 +10,12 @@ hash git >/dev/null 2>&1 || {
 
 ( if test -d $LOCAL; then
 	cd $LOCAL &&
+	git checkout . &&
 	env git pull --rebase --stat origin master
   else
 	env git clone --depth=1 $REMOTE $LOCAL
   fi ) &&
+sed -i '' 's,source \${0%/\*}/\.\.,source '$LOCAL',' $LOCAL/bin/realpath &&
 mkdir -p $HOME/bin && {
 	for file in $LOCAL/bin/*; do
 		ln -s $LOCAL/bin/${file##*/} bin/ 2>/dev/null || true
