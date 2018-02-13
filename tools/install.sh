7fae53fce428122f5a68977cfd0827cc4d0e36bc
Alberto Villa <villa.alberto@gmail.com>
2018-02-13 18:15:09 +0100

    Fix installation script

--- tools/install.sh
+++ tools/install.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-: ${REMOTE:=git@github.com:xzhavilla/xzhell.git}
+: ${REMOTE:=https://github.com/xzhavilla/xzhell.git}
 : ${LOCAL:=$HOME/.xzhell}
 
-hash git >/dev/null 2>&1 ||
-{ echo xzh: git: command not found 2>/dev/null
-  exit 127 }
+hash git >/dev/null 2>&1 || {
+	echo xzh: git: command not found 2>/dev/null
+	exit 127
+}
 
-env git clone --depth=1 $REMOTE $LOCAL &&
-mkdir -p $HOME/bin &&
-{ for file in $LOCAL/bin; do
-	ln -s $LOCAL/bin/$file bin/$file 2>/dev/null || true
-  done }
+( if test -d $LOCAL; then
+	cd $LOCAL &&
+	env git pull --rebase --stat origin master
+  else
+	env git clone --depth=1 $REMOTE $LOCAL
+  fi ) &&
+mkdir -p $HOME/bin && {
+	for file in $LOCAL/bin/*; do
+		ln -s $LOCAL/bin/${file##*/} bin/ 2>/dev/null || true
+	done
+}
