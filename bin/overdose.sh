baa39f2169c5deefedd80182dfc03975066a4c26
Alberto Villa <villa.alberto@gmail.com>
2018-09-07 15:27:07 +0200

    Add support for running commands into a Kubernetes pod

--- bin/overdose.sh
+++ bin/overdose.sh
@@ -6,11 +6,13 @@ PATH=$PATH:${SELF%/*}
 
 _x_extend _od_compose
 
-: ${OD_COMPOSEDIR:=.}
+: ${OD_DOCKERDIR:=.}
+: ${OD_K8SPOD:=}
 : ${OD_SH:=zsh bash}
 
 DOCKER=docker
 COMPOSE=docker-compose
+K8S=kubectl
 SHELLS=$(echo $OD_SH | awk -v RS=" " -v ORS="" '{ print "if which", $1, ">/dev/null 2>&1; then", $1 "; el" } END { print "se sh; fi" }')
 
 _od_docker() {
@@ -23,19 +25,57 @@ _od_docker() {
 
 _od_compose() {
 	test -n "$DRYRUN" &&
-	( echo "cd $OD_COMPOSEDIR &&"
+	( echo "cd $OD_DOCKERDIR &&"
 	  echo $COMPOSE "$@" ) >&2 &&
 	return
 
-	cd $OD_COMPOSEDIR &&
+	cd $OD_DOCKERDIR &&
 	$COMPOSE "$@"
 	status=$?
 	cd - >/dev/null
 	return $status
 }
 
+_od_k8s() {
+	test -n "$DRYRUN" &&
+	echo $K8S "$@" >&2 &&
+	return
+
+	$K8S "$@"
+}
+
+_od_pod() {
+	_x_min_args 1 $#
+
+	local pod=$1 pods npods
+
+	test -n "$DRYRUN" &&
+	echo $pod &&
+	return
+
+	printf 'Searching for pod "%s"...' "$pod" >&2
+	pods=$(_od_k8s get pods --no-headers | grep "^$pod" | cut -d " " -f 1)
+	npods=$(echo "$pods" | grep -c .)
+
+	test $npods -lt 1 &&
+	echo >&2 && _x_yell pod not found &&
+	return 1
+
+	test "$npods" -gt 1 &&
+	echo >&2 && _x_yell too many pods: $(echo $pods) &&
+	return 1
+
+	printf "\b\b\b: %s\n" "$pods" >&2
+	echo $pods
+}
+
 _od_exec() {
 	_x_min_args 1 $#
+
+	if test -n $OD_K8SPOD; then
+		_od_kexec "$OD_K8SPOD" "$@"
+		return $?
+	fi
 
 	local container=$1
 	shift
@@ -53,6 +93,26 @@ _od_exec() {
 	fi
 }
 
+_od_kexec() {
+	_x_min_args 2 $#
+
+	local pod=$1 container=$2
+	shift 2
+
+	pod=$(_od_pod $pod) || _x_die
+
+	if test $# -eq 0; then
+		_od_k8s exec $pod -it -c $container -- sh -c "$SHELLS"
+		return
+	fi
+
+	if _x_is_piped_in; then
+		_od_k8s exec $pod -i -c $container -- "$@"
+	else
+		_od_k8s exec $pod -it -c $container -- "$@"
+	fi
+}
+
 _od_shell() {
 	_od_exec $1
 }
@@ -62,11 +122,15 @@ _od_services() {
 }
 
 _opts() {
-	echo D:np:
+	echo D:K:np:
 }
 
 _opt_D() {
-	OD_COMPOSEDIR="$@"
+	OD_DOCKERDIR="$@"
+}
+
+_opt_K() {
+	OD_K8SPOD="$@"
 }
 
 _opt_n() {
@@ -118,8 +182,10 @@ _cmd_services() {
 	_od_services
 }
 
-_x_add_opt "-D COMPOSEDIR" \
-	"Home to docker-compose.yml [$(realpath $OD_COMPOSEDIR)]"
+_x_add_opt "-D DOCKERDIR" \
+	"Home to docker-compose.yml [$(realpath $OD_DOCKERDIR)]"
+_x_add_opt "-K K8SPOD" \
+	"Optional Kubernetes pod to interact with [${OD_K8SPOD:-none}]"
 _x_add_opt "-n" \
 	"Show commands without executing them"
 _x_add_opt "-p [NAMESPACE:]PLUGIN" \
